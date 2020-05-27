e4c5283f0c9219c2c6f680be54e86292bb99e5f3
caijiachen <cai1243752536@163.com>
2020-05-27 15:15:25 +0800

    。。

--- 项目源码和数据库/SQLQuery1.sql
+++ 项目源码和数据库/SQLQuery1.sql
@@ -1,14 +1,105 @@
-create table Heroes(
-Id int primary key identity(1,1),
-Name nvarchar(50) not null,
-Photo nvarchar(50),
-Q_Coin int not null,
-Weixin money
-)
-insert into Heroes(Name,Photo,Q_Coin,Weixin)
- values('ÍóºÀ','1.jpg',45,'£¤43.65'),
- ('ÁúÑªÎä¼§ Ï£ÍßÄÈ','2.jpg',35,'£¤43.95'),
- ('µÂ°î×Ü¹Ü ÕÔÐÅ','3.jpg',25,'£¤24.25'),
- ('ÕýÒå¾ÞÏñ ¼ÓÀï°Â','4.jpg',20,'£¤19.40')
+create database MoneyManage
+use MoneyManage
 
- select * from Heroes
+go
+
+create table Roles(
+ID int primary key identity(1,1),
+RoleName nvarchar(20)
+)
+
+create table Subsidy(
+ID int primary key identity(1,1),
+SubsidyName nvarchar(20),
+SubsidyDefault int
+)
+
+create table AttendanceSet(
+ID int primary key identity(1,1),
+ AttendanceName nvarchar(20),
+ PayDeduction int
+)
+
+create table Prize(
+ID int primary key identity(1,1),
+PrizeName nvarchar(20),
+PayDeductionDefault int
+)
+
+create table Department(
+ID int primary key identity(1,1),
+DepartmentName nvarchar(20)
+)
+
+create table Ranks(
+ID int primary key identity(1,1),
+RankName nvarchar(20),
+Basepay int
+)
+
+create table Shop(
+ID int primary key identity(1,1),
+ShopName nvarchar(20),
+Area nvarchar(50),
+States int,
+ShopManager nvarchar(20),
+Remarks nvarchar(50)
+)
+
+create table Personnel(
+ID int primary key identity(1,1),
+Pwd int,
+Name nvarchar(20),
+States int,
+RankID int references Ranks(ID) not null,
+RoleID int references Roles(ID) not null,
+DepartmentID int references Department(ID) not null,
+Sex bit,
+Addresses nvarchar(50),
+Photo int,
+ShopID int references Shop(ID) not null,
+Email nvarchar(50)
+)
+
+
+create table Subsidynotes(
+ID int primary key identity(1,1),
+PersonnelID int references Personnel(ID) not null,
+SubsidyID int references Subsidy(ID) not null,
+SubsidyDefault int,
+SubsidyDate date
+)
+
+create table Attendance(
+ID int primary key identity(1,1),
+PersonnelID int references Personnel(ID) not null,
+AttendanceSetID int references AttendanceSet(ID) not null,
+Counts int,
+Dates date
+)
+create table GetPrize(
+ID int primary key identity(1,1),
+PersonnelID int references Personnel(ID) not null,
+PrizeID int references Prize(ID) not null,
+Moneys Money,
+PrizeDate date
+)
+
+
+create table Pay(
+ID int primary key identity(1,1),
+PersonnelID int references Personnel(ID) not null,
+PersonnelName nvarchar(20),
+ShopName nvarchar(20),
+RankName nvarchar(20),
+PayDate date,
+Basepay int,
+AttendanceDebits int,
+PrizeMoney int,
+Payables int,
+Paidwages int,
+Remarks nvarchar(50)
+)
+
+drop table Personnel
+drop database MoneyManage
